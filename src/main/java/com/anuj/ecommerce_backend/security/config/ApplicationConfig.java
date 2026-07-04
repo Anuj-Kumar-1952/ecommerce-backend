dcd9469c476dcc85f528031a98ae9a67814d38b5
Anuj-Kumar-1952 <ak1952002@gmail.com>
2026-07-04 12:27:58 +0530

    feat: configure spring security and JWT authentication filter

--- src/main/java/com/anuj/ecommerce_backend/security/config/ApplicationConfig.java
+++ src/main/java/com/anuj/ecommerce_backend/security/config/ApplicationConfig.java
@@ -3,8 +3,13 @@ package com.anuj.ecommerce_backend.security.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.anuj.ecommerce_backend.security.service.CustomUserDetailsService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,12 +18,20 @@ import lombok.RequiredArgsConstructor;
 public class ApplicationConfig {
 
     @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(CustomUserDetailsService service, PasswordEncoder encoder) {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(service);
+        provider.setPasswordEncoder(encoder);
+
+        return provider;
     }
 }
